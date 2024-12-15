22edffe5167c234f80a671b0898b3092e1d43e18
Emerson Coskey <coskey@emerlabs.net>
2024-12-15 13:27:52 -0800

    add ext docs

--- src/ext.rs
+++ src/ext.rs
@@ -2,6 +2,7 @@ use bevy_app::App;
 
 use super::{GraphicsJob, SpecializedGraphicsJobPlugin};
 
+/// An extension trait for initializing graphics jobs on [`App`]
 pub trait InitGraphicsJobExt {
     fn init_graphics_job<J: GraphicsJob>(&mut self) -> &mut Self;
 }
